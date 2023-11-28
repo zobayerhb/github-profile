f07e2998c71437548ef1bcc86360df51834e05b4
unknown <hosenzobayer55@gmail.com>
2023-11-28 20:31:40 +0600

    git profile

---
+++ script.js
@@ -0,0 +1,75 @@
+const APIURL = "https://api.github.com/users/";
+
+const form = document.getElementById("form");
+const main = document.getElementById("main");
+const search = document.getElementById("search");
+
+async function getUser(username) {
+  const resp = await fetch(APIURL + username);
+  const respData = await resp.json();
+
+  createUserCard(respData);
+
+  getRepos(username);
+}
+
+async function getRepos(username) {
+  const resp = await fetch(APIURL + username + "/repos");
+  const respData = await resp.json();
+
+  addReposToCard(respData);
+}
+
+function createUserCard(user) {
+  const cardHTML = `
+        <div class="card">
+            <div>
+                <img class="avatar" src="${user.avatar_url}" alt="Not Found" />
+            </div>
+
+            <div class="user-info">
+                <h2>${user.name}</h2>
+                <p>${user.bio}</p>
+
+                <ul class="info">
+                    <li>${user.followers}<strong>followers</strong></li>
+                    <li>${user.following}<strong>following</strong></li>
+                    <li>${user.public_repos}<strong>repos</strong></li>
+                </ul>
+
+                <div id="repos"></div>
+            </div>
+        </div>
+  `;
+  main.innerHTML = cardHTML;
+}
+
+function addReposToCard(repos) {
+  const reposEl = document.getElementById("repos");
+
+  repos
+    .sort((a, b) => b.stargazers_count - a.stargazers_count)
+    .slice(0, 14)
+    .forEach((repo) => {
+      const repoEl = document.createElement("a");
+
+      repoEl.classList.add("repo");
+      repoEl.href = repo.html_url;
+      repoEl.target = "_blank";
+      repoEl.innerText = repo.name;
+
+      reposEl.appendChild(repoEl);
+    });
+}
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const user = search.value;
+
+  if (user) {
+    getUser(user);
+
+    search.value = "";
+  }
+});
